25aae716bd48097a9521f9d68b8373c34814c2a3
aswaa964@gmail.com <aswaa964@gmail.com>
2026-06-22 21:27:29 +0530

    Models created

---
+++ API/models/Merchant.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const merchantSchema = new mongoose.Schema(
+  {
+    shopName: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    ownerName: {
+      type: String,
+      required: true,
+      },
+      
+    registrationNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      },
+    address: {
+      type: String, },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Merchant", merchantSchema);
